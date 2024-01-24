a8bc2613eae5c340aba4543e1d0dd6f41ac4f7ac
girikgarg8 <girikgarg8@gmail.com>
2024-01-24 22:33:07 +0530

    Feat: Add Booking Model

---
+++ src/utils/common/enums.js
@@ -0,0 +1,15 @@
+const SEAT_TYPE = {
+    BUSINESS: 'business',
+    ECONOMY: 'economy',
+    PREMIUM_ECONOMY: 'premium_economy',
+    FIRST_CLASS: 'first_class'
+}
+
+const BOOKING_STATUS = {
+    BOOKED: 'booked',
+    CANCELLED: 'cancelled',
+    PENDING: 'pending',
+    INITIATED: 'initiated'
+};
+
+module.exports = { SEAT_TYPE, BOOKING_STATUS };
